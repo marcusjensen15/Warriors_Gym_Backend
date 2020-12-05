27435617bfdb0ec97e51ebb869b701e76064c31b
Marcus Jensen <marcusjensen1515@gmail.com>
2020-12-05 14:49:48 -0800

    specifying env port instead of hardcoded port.

--- index.js
+++ index.js
@@ -55,4 +55,6 @@ app.get('/users', (req, res) => {
 
 });
 
-app.listen(3000, () => console.log('listening on port 3000'));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log('listening on port ' + port));
