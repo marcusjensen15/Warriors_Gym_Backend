45e458c1d460723bcc55c6e20c2a645780c9ae13
Marcus Jensen <marcusjensen1515@gmail.com>
2020-12-22 16:45:05 -0800

    using lodash pick instead of listing out req.body repeatedly

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const _ = require('lodash');
 // const validateUser = require('../middleware/validateUser');
 const router = express.Router();
 const {User, validateUser} = require('../schema/userSchema');
@@ -45,15 +46,11 @@ router.post('/', async (req, res) => {
     let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('User already registered');
 
-    user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
 
     await user.save();
 
-    res.send(user);
+    res.send( _.pick(user, ['_id', 'name', 'email']));
 
     // const user = {
     //     id: usersArray.length + 1,
