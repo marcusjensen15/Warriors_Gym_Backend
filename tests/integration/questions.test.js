b73eb8ecb5f115d03628c96fadd230aacd1501b6
Marcus Jensen <marcusjensen1515@gmail.com>
2021-01-10 21:06:14 -0800

    first integration test passing

--- tests/integration/questions.test.js
+++ tests/integration/questions.test.js
@@ -6,9 +6,9 @@ describe('/questions', () => {
     beforeEach(() => { server = require('../../index'); });
     afterEach(() => { server.close(); });
     describe('GET /questions', () => {
-        it('should return all questions', async () => {
+        it('Should return error code 401 because we are trying to access the route with no token', async () => {
             const res = await request(server).get('/questions');
-            expect(res.status).toEqual(200);
+            expect(res.status).toEqual(401);
 
         });
     });
