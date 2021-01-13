b92bc1e1ff5901949e3b49480cb90c6476d81b2d
Marcus Jensen <marcusjensen1515@gmail.com>
2021-01-12 19:03:41 -0800

    test 401 failure for /questions/category is passing

--- tests/integration/questions.test.js
+++ tests/integration/questions.test.js
@@ -2,6 +2,8 @@ const request = require('supertest');
 
 let server;
 
+// rite test first for all 401 errors
+
 describe('/questions', () => {
     beforeEach(() => { server = require('../../index'); });
     afterEach(() => { server.close(); });
@@ -9,7 +11,18 @@ describe('/questions', () => {
         it('Should return error code 401 because we are trying to access the route with no token', async () => {
             const res = await request(server).get('/questions');
             expect(res.status).toEqual(401);
+        });
 
+
+    });
+
+
+    describe('GET /questions/:category', () => {
+        it('Should return error code 401 because we are trying to access the route with no token', async () => {
+            const res = await request(server).get('/questions/category');
+            expect(res.status).toEqual(401);
         });
     });
+
+
 });
