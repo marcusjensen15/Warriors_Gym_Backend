4e3faa44044775e5b93b9972204f8357ef400d7d
Marcus Jensen <marcusjensen1515@gmail.com>
2021-01-12 19:54:53 -0800

    All /users routes 401 failure tests passing

--- tests/integration/questions.test.js
+++ tests/integration/questions.test.js
@@ -2,11 +2,12 @@ const request = require('supertest');
 
 let server;
 
-// rite test first for all 401 errors
+// Write test first for all 401 errors
 
-describe('/questions', () => {
+describe('Questions Routes', () => {
     beforeEach(() => { server = require('../../index'); });
     afterEach(() => { server.close(); });
+
     describe('GET /questions', () => {
         it('Should return error code 401 because we are trying to access the route with no token', async () => {
             const res = await request(server).get('/questions');
@@ -20,31 +21,32 @@ describe('/questions', () => {
 
 
     });
-});    
+ 
 
-describe('/questions/:category', () => {
-    it('Should return error code 401 because we are trying to access the route with no token', async () => {
-        const res = await request(server).get('/questions/category');
-        expect(res.status).toEqual(401);
-    });
-});
-
-describe('/questions/:category/:id', () => {
-    it('GET: Should return error code 401 because we are trying to access the route with no token', async () => {
-        const res = await request(server).get('/questions/category/1234');
-        expect(res.status).toEqual(401);
+    describe('/questions/:category', () => {
+        it('Should return error code 401 because we are trying to access the route with no token', async () => {
+            const res = await request(server).get('/questions/category');
+            expect(res.status).toEqual(401);
+        });
     });
 
-    it('DELETE: Should return error code 401 because we are trying to access the route with no token', async () => {
-        const res = await request(server).delete('/questions/category/1234');
-        expect(res.status).toEqual(401);
-    });
+    describe('/questions/:category/:id', () => {
+        it('GET: Should return error code 401 because we are trying to access the route with no token', async () => {
+            const res = await request(server).get('/questions/category/1234');
+            expect(res.status).toEqual(401);
+        });
 
-    it('POST: Should return error code 401 because we are trying to access the route with no token', async () => {
-        const payload = {questionText: "Here is some text", type: "type", category: "category", possibleAnswers: ["some", "possible", "answers"], correctAnswer: "answers"}
-        const res = await request(server).put('/questions/category/1234').send(payload);
-        expect(res.status).toEqual(401);
-    });
+        it('DELETE: Should return error code 401 because we are trying to access the route with no token', async () => {
+            const res = await request(server).delete('/questions/category/1234');
+            expect(res.status).toEqual(401);
+        });
 
+        it('POST: Should return error code 401 because we are trying to access the route with no token', async () => {
+            const payload = {questionText: "Here is some text", type: "type", category: "category", possibleAnswers: ["some", "possible", "answers"], correctAnswer: "answers"}
+            const res = await request(server).put('/questions/category/1234').send(payload);
+            expect(res.status).toEqual(401);
+        });
+
+    });
 });
 
