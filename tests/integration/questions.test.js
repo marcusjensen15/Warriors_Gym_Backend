7b60ce945a39a952f122786f8861407b731020a5
Marcus Jensen <marcusjensen1515@gmail.com>
2021-01-12 19:19:06 -0800

    test 401 failure for all questions related routes

--- tests/integration/questions.test.js
+++ tests/integration/questions.test.js
@@ -12,22 +12,39 @@ describe('/questions', () => {
             const res = await request(server).get('/questions');
             expect(res.status).toEqual(401);
         });
+        it('Should return error code 401 because we are trying to access the route with no token', async () => {
+            const payload = {questionText: "Here is some text", type: "type", category: "category", possibleAnswers: ["some", "possible", "answers"], correctAnswer: "answers"}
+            const res = await request(server).post('/questions').send(payload);
+            expect(res.status).toEqual(401);
+        });
 
 
     });
 });    
 
-describe('GET /questions/:category', () => {
+describe('/questions/:category', () => {
     it('Should return error code 401 because we are trying to access the route with no token', async () => {
         const res = await request(server).get('/questions/category');
         expect(res.status).toEqual(401);
     });
 });
 
-describe('GET /questions/:category/:id', () => {
-    it('Should return error code 401 because we are trying to access the route with no token', async () => {
+describe('/questions/:category/:id', () => {
+    it('GET: Should return error code 401 because we are trying to access the route with no token', async () => {
         const res = await request(server).get('/questions/category/1234');
         expect(res.status).toEqual(401);
     });
+
+    it('DELETE: Should return error code 401 because we are trying to access the route with no token', async () => {
+        const res = await request(server).delete('/questions/category/1234');
+        expect(res.status).toEqual(401);
+    });
+
+    it('POST: Should return error code 401 because we are trying to access the route with no token', async () => {
+        const payload = {questionText: "Here is some text", type: "type", category: "category", possibleAnswers: ["some", "possible", "answers"], correctAnswer: "answers"}
+        const res = await request(server).put('/questions/category/1234').send(payload);
+        expect(res.status).toEqual(401);
+    });
+
 });
 
