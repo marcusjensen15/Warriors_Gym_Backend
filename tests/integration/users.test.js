94684c29c47da6a75867dd927f2b9e3453e84d5e
Marcus Jensen <marcusjensen1515@gmail.com>
2021-01-17 12:33:40 -0800

    eleminate unncessary }); within users.test.js that was causing errors because the sever wasn't shutting down

--- tests/integration/users.test.js
+++ tests/integration/users.test.js
@@ -18,14 +18,15 @@ describe('All Users Routes', () => {
             const res = await request(server).get('/users/me');
             expect(res.status).toEqual(401);
         });
-    });
+   
         
         it('PUT: Should return error code 401 because we are trying to access the route with no token', async () => {
             const payload = {email: "test@email.com", password: "123454"}
             const res = await request(server).put('/questions').send(payload);
             expect(res.status).toEqual(401);
-    });
+        });
 
+    });
     describe('/users/:id', () => {
         it('DELETE: Should return error code 401 because we are trying to access the route with no token', async () => {
             const res = await request(server).delete('/users/1234');
