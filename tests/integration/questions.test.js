2a1d71b8857c1f91e8cb068e35f35c49d0b72579
Marcus Jensen <marcusjensen1515@gmail.com>
2021-01-31 19:09:06 -0800

    remove unnecessary comments.

--- tests/integration/questions.test.js
+++ tests/integration/questions.test.js
@@ -290,28 +290,5 @@ describe('All questions routes', () => {
     });
 
 
-
-
-
-
-
-
-    // describe('/questions/:category/:id', () => {
-    //     it('GET: Should return error code 401 because we are trying to access the route with no token', async () => {
-    //         const res = await request(server).get('/questions/category/1234');
-    //         expect(res.status).toEqual(401);
-    //     });
-
-    //     it('DELETE: Should return error code 401 because we are trying to access the route with no token', async () => {
-    //         const res = await request(server).delete('/questions/category/1234');
-    //         expect(res.status).toEqual(401);
-    //     });
-
-    //     it('POST: Should return error code 401 because we are trying to access the route with no token', async () => {
-    //         const payload = {questionText: "Here is some text", type: "type", category: "category", possibleAnswers: ["some", "possible", "answers"], correctAnswer: "answers"}
-    //         const res = await request(server).put('/questions/category/1234').send(payload);
-    //         expect(res.status).toEqual(401);
-    //     });
-    // });
 });
 
